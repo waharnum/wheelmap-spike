a52a46a281d649b4ec3632547fbc129c2d3d5a1a
Alan Harnum <aharnum@ocadu.ca>
2018-02-01 17:22:31 -0500

    Replace iframe each time reloading.

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
+// Not a lot of work here yet, but this would be for writing
+// server-side client code
+
 var apiKey = process.env.WHEELMAP_KEY;
 if(apiKey) {
     var Client = require("node-rest-client").Client;
 
     var client = new Client();
 
-    
 } else {
     console.log("Please set API key to WHEELMAP_KEY env variable");
     process.exit(1);
