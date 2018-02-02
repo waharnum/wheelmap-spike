89c7a5b7b176012710ca9c23f269cdc38a222e03
Alan Harnum <aharnum@ocadu.ca>
2018-02-02 13:13:04 -0500

    REST client for accessibility.cloud

--- index.js
+++ index.js
@@ -1,13 +1,93 @@
 // Not a lot of work here yet, but this would be for writing
 // server-side client code
+"use strict";
 
-var apiKey = process.env.WHEELMAP_KEY;
-if(apiKey) {
-    var Client = require("node-rest-client").Client;
+var fluid = require("infusion");
+var Client = require("node-rest-client").Client;
+var client = new Client();
 
-    var client = new Client();
+var bigidea = fluid.registerNamespace("bigidea");
 
-} else {
-    console.log("Please set API key to WHEELMAP_KEY env variable");
-    process.exit(1);
-}
+// Wrapper for https://www.npmjs.com/package/node-rest-client
+fluid.defaults("bigidea.restClient", {
+    gradeNames: ["fluid.component"],
+    restConfig: {
+        url: "",
+        headers: {
+        },
+        parameters: {
+        },
+        path: {
+        },
+        data: {
+        },
+        requestConfig: {            
+        },
+        responseConfig: {
+        }
+    },
+    listeners: {
+        "onCreate.initializeClient": {
+            funcName: "bigidea.restClient.initializeClient"
+        }
+    },
+    invokers: {
+        get: {
+            funcName: "bigidea.restClient.get",
+            args: ["{that}"]
+        }
+    }
+});
+
+bigidea.restClient.initializeClient = function (that) {
+    that.client = new Client();
+};
+
+bigidea.restClient.get = function(that) {
+
+    var args = {
+        headers: that.options.restConfig.headers,
+        parameters: that.options.restConfig.parameters,
+        path: that.options.restConfig.path,
+        data: that.options.restConfig.data
+    };
+
+    var promise = fluid.promise();
+
+    client.get(that.options.restConfig.url, args, function (data, response) {
+        promise.resolve(
+            {
+                data: data,
+                response: response}
+        );
+    });
+
+    return promise;
+};
+
+var apiKey = process.env.ACCESSIBILITY_CLOUD_KEY;
+
+var acClient = bigidea.restClient({
+    restConfig: {
+        url: "https://www.accessibility.cloud/place-infos.json",
+        headers: {
+            "X-App-Token": apiKey,
+            "Accept": "application/json"
+        },
+        parameters: {
+            latitude: 48.2435,
+            longitude: 16.3974,
+            accuracy: 1000
+        }
+    },
+});
+
+var getPromise = acClient.get();
+
+    getPromise.then(
+        function (response) {
+            console.log(response);
+        },
+        function (error) {
+            console.log(error);
+        });
